f9953f41037a268fb65ab6b975b8bef745460253
adrien <adriengaucher02@gmail.com>
2026-08-08 17:39:33 +0200

    add config exemple

---
+++ config.exemple.php
@@ -0,0 +1,10 @@
+<?php
+
+define('SMTP_HOST', '');
+define('SMTP_PORT', 587);
+define('SMTP_SECURE', '');
+define('SMTP_USER', '');
+define('SMTP_PASS', '');
+define('FROM_EMAIL', '');
+define('FROM_NAME', '');
+define('TO_EMAIL', '');
