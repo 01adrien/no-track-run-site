36aa7d46bf8577cf2ea45b03a75d1cd7ee0d9145
adrien <adriengaucher02@gmail.com>
2026-09-05 01:42:05 +0200

    fix allowed domain

--- public/contact.php
+++ public/contact.php
@@ -51,7 +51,7 @@ $allowedHosts = [
     'notrackrun.eu',
     'www.notrackrun.eu',
     'test.notrckrun.eu',
-    'www.test.notrckrun.eu',
+    'www.test.notrackrun.eu',
     'localhost',
     '127.0.0.1',
     'dev.notrackrun.eu',
